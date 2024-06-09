0563dae8cba3af3e9ddee763f1d2d55dd7c9072f
Keshav Jha <keshavjha0610@gmail.com>
2024-06-09 13:19:25 +0530

    error handling in layouts

--- routing-demo/src/app/products/[productId]/layout.tsx
+++ routing-demo/src/app/products/[productId]/layout.tsx
@@ -1,8 +1,15 @@
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count)
+}
+
 export default function ProductDetailLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const random = getRandomInt(2)
+    if (random === 1)
+        throw new Error("Error loading product")
     return (
         <>
             {children}
