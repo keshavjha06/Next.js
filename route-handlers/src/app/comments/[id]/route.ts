67b0f1c53db0615e2b1301da39df3a5005248efa
Keshav Jha <keshavjha0610@gmail.com>
2024-06-10 10:00:07 +0530

    Redirects in route handlers

--- route-handlers/src/app/comments/[id]/route.ts
+++ route-handlers/src/app/comments/[id]/route.ts
@@ -1,7 +1,11 @@
 import { comments } from "../data"
+import { redirect } from "next/navigation"
 
 export async function GET(_request: Request,
     { params }: { params: { id: string } }) {
+    if (parseInt(params.id) > comments.length) {
+        redirect("/comments")
+    }
     const comment = comments.find(
         (comment) => comment.id === parseInt(params.id)
     )
