ea653974fa029b8608ff971a279c0392743b66f2
Keshav <keshav@forge.gg>
2024-08-11 11:17:16 +0530

    client-only code

--- rendering/src/app/client-route/page.tsx
+++ rendering/src/app/client-route/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { useTheme } from "@/components/theme-provider";
+import { clientSideFunction } from "@/utils/client-utils";
 // import "./ImageSlider.css";
 
 export default function ClientRoutePage() {
     const theme = useTheme()
-    const settings = {
-        dots: true,
-    };
+    const result = clientSideFunction()
     return (
         <>
             <h1
@@ -18,10 +15,10 @@ export default function ClientRoutePage() {
                     color: theme.colors.secondary,
                 }}
             >
-                Client Route
+                Client Route {result}
             </h1>
             <div className="image-slider-container">
-                <Slider {...settings}>
+                <Slider>
                     <div>
                         <img src="https://picsum.photos/400/200" />
                     </div>
--- rendering/src/app/server-route/page.tsx
+++ rendering/src/app/server-route/page.tsx
@@ -1,10 +1,11 @@
 import { ImageSlider } from "@/components/ImageSlider";
+import { clientSideFunction } from "@/utils/client-utils";
 import { serverSideFunction } from "@/utils/server-utils"
 
 export default function ServerRoutepage() {
     console.log("server route rendered");
-
     const result = serverSideFunction()
+    const clientResult = clientSideFunction()
     return (
         <>
             <h1>ServerRoutePage</h1>
